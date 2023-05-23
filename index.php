ab58764072d4b9d72aefb5281a43a95263b81c6a
Anlimah <y.m.ratty7@gmail.com>
2023-05-23 11:25:43 +0000

    Trying the GET request

--- index.php
+++ index.php
@@ -3,17 +3,17 @@ require_once('bootstrap.php');
 
 use Src\Controller\USSDHandler;
 
-if ($_SERVER["REQUEST_METHOD"] != "POST") die("Invalid request!");
+//if ($_SERVER["REQUEST_METHOD"] != "POST") die("Invalid request!");
 
 $sessionId      = $_POST["session_id"];     // Session ID
-$serviceCode    = $_POST["service_code"];   // Service code
 $phoneNumber    = $_POST["msisdn"];         // Phone number
+$msgType        = $_POST["msg_type"];       // Message Type 0, 1, 2
 $ussdBody       = $_POST["ussd_body"];      // response ussdBody
 $networkCode    = $_POST["nw_code"];        // network code 01 > MTN, 02 > VODA, 
-$msgType        = $_POST["msg_type"];       // Message Type 0, 1, 2
+$serviceCode    = $_POST["service_code"];   // Service code
 
 $ussd = new USSDHandler($sessionId, $serviceCode, $phoneNumber, $ussdBody, $networkCode, $msgType);
-$ussd->requestLogger(json_encode($_POST));
+$ussd->requestLogger(json_encode($_GET));
 $response = $ussd->control();
 
 header("Content-Type: application/json");
