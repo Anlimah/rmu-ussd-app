7f661e67f80e887a7cbf69e4837c5e98d2e991ab
Anlimah <y.m.ratty7@gmail.com>
2023-05-23 11:35:26 +0000

    Trying other status code

--- index.php
+++ index.php
@@ -3,18 +3,27 @@ require_once('bootstrap.php');
 
 use Src\Controller\USSDHandler;
 
-//if ($_SERVER["REQUEST_METHOD"] != "POST") die("Invalid request!");
+switch ($_SERVER["REQUEST_METHOD"]) {
+    case 'POST':
+        $sessionId      = $_POST["session_id"];     // Session ID
+        $phoneNumber    = $_POST["msisdn"];         // Phone number
+        $msgType        = $_POST["msg_type"];       // Message Type 0, 1, 2
+        $ussdBody       = $_POST["ussd_body"];      // response ussdBody
+        $networkCode    = $_POST["nw_code"];        // network code 01 > MTN, 02 > VODA, 
+        $serviceCode    = $_POST["service_code"];   // Service code
 
-$sessionId      = $_POST["session_id"];     // Session ID
-$phoneNumber    = $_POST["msisdn"];         // Phone number
-$msgType        = $_POST["msg_type"];       // Message Type 0, 1, 2
-$ussdBody       = $_POST["ussd_body"];      // response ussdBody
-$networkCode    = $_POST["nw_code"];        // network code 01 > MTN, 02 > VODA, 
-$serviceCode    = $_POST["service_code"];   // Service code
+        $ussd = new USSDHandler($sessionId, $serviceCode, $phoneNumber, $ussdBody, $networkCode, $msgType);
+        $ussd->requestLogger(json_encode($_POST));
+        $response = $ussd->control();
 
-$ussd = new USSDHandler($sessionId, $serviceCode, $phoneNumber, $ussdBody, $networkCode, $msgType);
-$ussd->requestLogger(json_encode($_GET));
-$response = $ussd->control();
+        header("Content-Type: application/json");
+        break;
 
-header("Content-Type: application/json");
-echo json_encode($response);
+    default:
+        header("HTTP/1.1 403 Forbidden");
+        header("Content-Type: text/plain");
+        $response;
+        break;
+}
+
+echo $response;
