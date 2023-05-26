d4d3a0acfd53b3b29080562eca4e51cec78e57f1
Anlimah <y.m.ratty7@gmail.com>
2023-05-26 20:21:05 +0000

    Working on new trends

--- index.php
+++ index.php
@@ -1,24 +1,14 @@
 <?php
-session_start();
-$_SESSION["level"] = array();
 
 require_once('bootstrap.php');
 
 use Src\Controller\USSDHandler;
 
-$response;
-
 switch ($_SERVER["REQUEST_METHOD"]) {
     case 'POST':
         $_POST = json_decode(file_get_contents("php://input"), true);
-        $sessionId      = $_POST["session_id"];     // Session ID
-        $phoneNumber    = $_POST["msisdn"];         // Phone number
-        $msgType        = $_POST["msg_type"];       // Message Type 0, 1, 2
-        $ussdBody       = $_POST["ussd_body"];      // response ussdBody
-        $networkCode    = $_POST["nw_code"];        // network code 01 > MTN, 02 > VODA, 
-        $serviceCode    = $_POST["service_code"];   // Service code
 
-        $ussd = new USSDHandler($sessionId, $phoneNumber, $msgType, $serviceCode, $ussdBody, $networkCode);
+        $ussd = new USSDHandler($_POST);
         $response = $ussd->control();
 
         header("Content-Type: application/json");
